f6e01b749b7fb8785baf62441b20795104e72341
Hugo Martinez <le19080710@itmerida.edu.mx>
2024-05-04 21:42:55 -0600

    Add .env file and update index.php with database configuration

--- 15-abr-30/index.php
+++ 15-abr-30/index.php
@@ -19,5 +19,18 @@
     </form>
     </div>
   </div>
+  <?php
+  $env = parse_ini_file('.env');
+  $DB_HOST = $env['DB_HOST'];
+  $DB_USER = $env['DB_USER'];
+  $DB_PASS = $env['DB_PASS'];
+  $DB_NAME = $env['DB_NAME'];
+
+  echo "DB_HOST: $DB_HOST <br>";
+  echo "DB_USER: $DB_USER <br>";
+  echo "DB_PASS: $DB_PASS <br>";
+  echo "DB_NAME: $DB_NAME <br>";
+  
+  ?>
 </body>
 </html>
